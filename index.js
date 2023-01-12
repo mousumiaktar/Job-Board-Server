e08d001239ef226fc5e4d9da38739baa6124ac12
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2023-01-12 12:39:19 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,6 +10,19 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ayosb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+      
+  
+  
+    } finally {
+    }
+  };
+  
+  run().catch((err) => console.log(err));
 
 
 app.get("/", (req, res) => {
